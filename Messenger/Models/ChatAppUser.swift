50e2dbac4808e570deb8bc1ea67357cb63e0a660
Mark Nagibin <myrkwill@icloud.com>
2021-06-14 22:59:44 +0300

    Move models from view controllers

---
+++ Messenger/Models/ChatAppUser.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+    
+    var profilePictureFileName: String {
+        return "\(safeEmail)_profile_picture.png"
+    }
+}
